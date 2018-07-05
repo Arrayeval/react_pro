cc691306ea3d5395b06b9d9f79d29bc8b0b6f11d
EvalGitHub <957010831@qq.com>
2018-07-06 00:40:44 +0800

    add file header

--- react_pro/src/components/pc/modulePart.js
+++ react_pro/src/components/pc/modulePart.js
@@ -1,10 +1,20 @@
 //  首页模块
  import React, {Component} from 'react'
+ import '../../scss/pcStyle/modulePart.scss'
  class moduleParts extends Component {
    render () {
     return (
       <div className="module-part-wrapper">
-       
+        <div className="config-wrapper">
+          <div className="config-area">
+            <span className="add-btn inline-block">+ Add</span>
+            <span className="login-out-area">
+              <span className="change-item"> </span>
+              <span className="change-item"></span>
+              <span className="login-out-item">安全退出</span>
+            </span>
+          </div>
+        </div>
       </div>
     )
    }
