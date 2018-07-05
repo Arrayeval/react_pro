4a0b57127ab83e6d04ef453e21d9610afef6e87e
evel.yi <evel.yi@yff.com>
2018-07-05 18:58:06 +0800

    update

--- react_pro/src/components/pc/modulePart.js
+++ react_pro/src/components/pc/modulePart.js
@@ -1,11 +1,10 @@
+//  首页模块
  import React, {Component} from 'react'
  class moduleParts extends Component {
    render () {
     return (
-      <div className='header-wrapper'>
-        <div className="inner-wrapper">
-         this is moduleParts
-        </div>
+      <div className="module-part-wrapper">
+       
       </div>
     )
    }
