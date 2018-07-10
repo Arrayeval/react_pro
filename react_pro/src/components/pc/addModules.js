6625c57dd06fca0ec580a26468582f06eb337001
evel.yi <evel.yi@yff.com>
2018-07-10 19:20:23 +0800

    update

--- react_pro/src/components/pc/addModules.js
+++ react_pro/src/components/pc/addModules.js
@@ -8,14 +8,27 @@ const { MonthPicker, RangePicker, WeekPicker } = DatePicker
 class addModules extends Component {
     constructor(props){
         super(props)
-        console.log(props)
         this.state= {
             mark:'我从addModules中来',
-            startValue: new Date()
+            title:'',
+            shortDes:'',
+            creator:'',
+            createTime:''
         }
     };
+    // 设置时间
     onChange (date, dateString) {
-        console.log(date, dateString);
+        this.setState({createTime:dateString})
+    }
+    handelVal (event) {
+        let _name = event.target.name
+        let _obj = {};
+        _obj[_name] = event.target.value
+         this.setState({..._obj});
+    }
+    // 数据提交
+    dealData () {
+        console.log(this.state)
     }
     render () {
         return (
@@ -24,47 +37,46 @@ class addModules extends Component {
               <Link to='/' className="back-btn" tag="button">back</Link>
             </p>
             <div className="module-form">
-             <p className="item-form">
+             <div className="item-form">
                 <label className="self-lebal">模块名：</label>
-                <input type="text" placeholder="请输入文章标题" className="self-input"/>
-             </p>
-             <p className="item-form"> 
+                <input type="text" placeholder="请输入文章标题" className="self-input" name="title" value={this.state.title}  onChange={this.handelVal.bind(this)}/>
+             </div>
+             <div className="item-form"> 
                 <label className="self-lebal textarea-lebal">简单描述：</label>
-                <textarea type="text" placeholder="随便写写这模块是干什么的..." className="self-textarea"></textarea>
-             </p>
-            
-             <p className="item-form">
+                <textarea type="text" placeholder="随便写写这模块是干什么的..." className="self-textarea" name="shortDes" value={this.state.shortDes} onChange={this.handelVal.bind(this)}></textarea>
+             </div>
+             <div className="item-form">
                 <label className="self-lebal">创建时间：</label>
                 {/* <input type="text" placeholder="请输入文章标题" className="self-input"/> */}
-                <DatePicker onChange={this.onChange.bind(this)}  
+                <DatePicker 
                     format="YYYY-MM-DD HH:mm:ss"
+                    onChange={this.onChange.bind(this)}
                     placeholder="创建时间..."/>
-             </p>
-             <p className="item-form">
+             </div>
+             <div className="item-form">
                 <label className="self-lebal">创建作者：</label>
-                <input type="text" placeholder="我们需要一个作者" className="self-input"/>
-             </p>
-             <p className="item-form">
+                <input type="text" placeholder="我们需要一个作者" className="self-input" name="creator" value={this.state.creator} onChange={this.handelVal.bind(this)}/>
+             </div>
+             <div className="item-form">
                 <label className="self-lebal">模块logo：</label>
                 <span className="add-btn vertical-top"></span>
                 {/* <input type="text" placeholder="请输入文章标题" className="self-input"/> */}
-             </p>
+             </div>
+             <div className="item-form">
+               <button onClick={this.dealData.bind(this)}>提交</button>
+             </div>
             </div>
-          
-               
                 {/* 
                 <Link to={`${this.props.match.url}/ee/22`} className="clearfix">detailBtn</Link>
                 <Route exact path={`${this.props.match.url}`} component={detail}></Route>
                 <Route path={`${this.props.match.url}/ee/:id`} component={detail}></Route>
                 <Link to='/' className="clearfix">addModule</Link> */}
-
             </div>
         )
     } 
 }
 
 export default addModules;
-
 // const addModules = ({match}) => 
 //          (
 //             <div className="add-module-wrapper">
@@ -73,5 +85,4 @@ export default addModules;
 //                 <Route path={`${match.url}/ees`} component={detail}></Route>
 //             </div>
 //         )
-    
 // export default addModules;
