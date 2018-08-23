352886a2fb660ce94f2ce87597da58eec5ca7726
evel.yi <evel.yi@yff.com>
2018-08-23 10:06:10 +0800

    add redux react-redux

---
+++ react_pro/src/redux/data.js
@@ -0,0 +1,15 @@
+// 初始数据
+export const tmpData = {
+  cart: [
+    {
+      product: 'bread 700g',
+      quantity: 2,
+      unitCost: 90
+    },
+    {
+      product: 'milk 500ml',
+      quantity: 1,
+      unitCost: 47
+    }
+  ]
+}
