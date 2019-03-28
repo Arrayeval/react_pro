ff62f08a1df2a7fe9bfb7b798cebfe6615e199aa
EvalGitHub <957010831@qq.com>
2019-03-28 23:54:33 +0800

    TODO

--- react_pro/src/service/user/index.js
+++ react_pro/src/service/user/index.js
@@ -4,6 +4,9 @@ export const UserApi = {
     login (obj) {
         let url = webApi.urlList.userHandle.login
         return axios.post(url, obj)
+    },
+    loginOUt () {
+        // 每个用户都应该返回一个唯一id ,便于登出
     }
 
 }
