e41438eb369e57368ddbb4b0f483d7062cea0e88
EvalGitHub <957010831@qq.com>
2019-07-16 11:51:05 +0800

    添加参考链接

--- src/saga/index.js
+++ src/saga/index.js
@@ -1,9 +1,16 @@
+/**
+ * 参考链接：
+ * https://redux-saga-in-chinese.js.org/
+ * https://www.cnblogs.com/mengff/p/9505474.html
+ * https://www.jianshu.com/p/
+ * https://www.jianshu.com/p/e84493c7af35
+ **/ 
 import {call, put, takeEvery, takeLatest} from 'redux-saga/effects'
 import axios from 'axios'
 
 import {BEGIN_GET_POSTS} from '../redux/actionType'
 import {get_errors, get_users} from '../redux/action'
-function* showPostsAsync (action) {
+function* showPostsAsync () {
     try {
         const response = yield call(axios.get, 'https://jsonplaceholder.typicode.com/posts')
         yield put(get_users(response.data))
